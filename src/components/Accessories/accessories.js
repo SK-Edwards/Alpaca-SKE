b90542879832cd9b39470bea77b8d735f42f2826
SK. Edwards <125165568+SK-Edwards@users.noreply.github.com>
2023-07-06 14:57:04 -0400

    Started with the file structuring and a few lines of code.

---
+++ src/components/Accessories/accessories.js
@@ -0,0 +1,9 @@
+import styles from "./accessories.css"
+
+const Accessories = ({img}) => {
+    return (
+        <img className={styles.absulute} src={img} alt="accssories"/>
+    )
+}
+
+export default Accessories;
